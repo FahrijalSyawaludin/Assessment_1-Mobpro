23997303e5e68f48b6b2d24130aeef8c694f9f6d
FahrijalSyawaludin <fahrijals@student.telkomuniversity.ac.id>
2023-03-30 16:40:18 +0700

    Initial Commit

--- app/src/main/java/org/d3if3040/ritdhafood/LoginActivity.kt
+++ app/src/main/java/org/d3if3040/ritdhafood/LoginActivity.kt
@@ -1,63 +1,78 @@
 package org.d3if3040.ritdhafood
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import org.d3if3040.ritdhafood.databinding.LoginLayoutBinding
 
+
 class LoginActivity : AppCompatActivity() {
 
+    companion object {
+        const val SHARED_PREF_NAME = "myPref"
+    }
+
     private lateinit var binding: LoginLayoutBinding
+    private lateinit var db: DataBaseHeleperLogin
+
+    //shared pref
+    private val SHARED_PREF_NAME = "myPref"
+    private lateinit var sharedPreferences: SharedPreferences
+
+    @SuppressLint("MissingInflatedId")
+    @Override
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = LoginLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar) // set the Toolbar as the ActionBar
-        supportActionBar?.setDisplayHomeAsUpEnabled(true) // enable the Up button
+        db = DataBaseHeleperLogin(this)
 
-        // set the click listener for the Up button
-        binding.toolbar.setNavigationOnClickListener {
-            onBackPressed()
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE)
+
+        binding.tvRegister.setOnClickListener {
+            Register.newInstance().show(supportFragmentManager, Register.TAG)
         }
 
-        binding.loginButton.setOnClickListener {
-            val email = binding.emailEdittext.text.toString()
-            val password = binding.passwordEdittext.text.toString()
+        binding.btnLogin.setOnClickListener {
+            val getUsername = binding.etUsername.text.toString()
+            val getPassword = binding.etPassword.text.toString()
 
-            // Validate input
-            if (email.isEmpty() || password.isEmpty()) {
-                // Show error message
-                binding.errorMessage.text = getString(R.string.error_message_empty_fields)
-                return@setOnClickListener
-            }
-
-            // Authenticate user
-            val authenticated = authenticateUser(email, password)
-
-            if (authenticated) {
-                // Save user data to SharedPreferences
-                val sharedPreferences = getSharedPreferences("userData", MODE_PRIVATE)
-                val editor = sharedPreferences.edit()
-                editor.putString("email", email)
-                editor.putString("password", password)
-                editor.apply()
-
-                // Navigate to home screen
-                val intent = Intent(this, HomeActivity::class.java)
-                startActivity(intent)
-                finish() // Prevent the user from going back to the login screen
+            if (getPassword.isEmpty() && getPassword.isEmpty()) {
+                Toast.makeText(
+                    applicationContext,
+                    "Username atau password salah!",
+                    Toast.LENGTH_LONG
+                ).show()
             } else {
-                // Show error message
-                binding.errorMessage.text = getString(R.string.error_message_empty_fields)
+                val masuk = db.checkLogin(getUsername, getPassword)
+                if (masuk) {
+                    val updateSession = db.upgradeSession("ada", 1)
+                    if (updateSession) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Berhasil Masuk",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        val editor = sharedPreferences.edit()
+                        editor.putBoolean("masuk", true)
+                        editor.apply()
+                        val dashbord = Intent(applicationContext, MainActivity::class.java)
+                        startActivity(dashbord)
+                        finish()
+                    }
+                } else {
+                    Toast.makeText(
+                        applicationContext,
+                        "Gagal Masuk",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
             }
         }
-    }
-
-    private fun authenticateUser(email: String, password: String): Boolean {
-        // Replace this with your authentication logic
-        return email == "cps@gmail.com" && password == "fahri123"
     }
 }
--- app/src/main/java/org/d3if3040/ritdhafood/MainActivity.kt
+++ app/src/main/java/org/d3if3040/ritdhafood/MainActivity.kt
@@ -1,25 +1,47 @@
 package org.d3if3040.ritdhafood
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import android.widget.Toast
 import org.d3if3040.ritdhafood.databinding.ActivityMainBinding
-import org.d3if3040.ritdhafood.databinding.LoginLayoutBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var db: DataBaseHeleperLogin
+    private lateinit var sharedPreferences: SharedPreferences
+
+    companion object {
+        const val SHARED_PREF_NAME = "myPref"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonLogin.setOnClickListener {
-            CoroutineScope(Dispatchers.Main).launch {
-                val loginBinding = LoginLayoutBinding.inflate(layoutInflater)
-                setContentView(loginBinding.root)
+        db = DataBaseHeleperLogin(this)
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
+
+        val checkSession = db.checkSession("ada")
+        if (!checkSession) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        binding.btnKeluar.setOnClickListener {
+            val updateSession = db.upgradeSession("kosong", 1)
+            if (updateSession) {
+                Toast.makeText(this, "Berhasil Keluar", Toast.LENGTH_LONG).show()
+
+                val editor = sharedPreferences.edit()
+                editor.putBoolean("masuk", false)
+                editor.apply()
+
+                val keluar = Intent(this, LoginActivity::class.java)
+                startActivity(keluar)
+                finish()
             }
         }
     }
